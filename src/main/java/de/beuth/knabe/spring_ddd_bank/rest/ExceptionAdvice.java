53f5ed6d1cdaef5b3d10d8476041f78d2c175690
Christoph Knabe <knabe@beuth-hochschule.de>
2017-09-28 09:50:43 +0200

    Take the ResourceBundle for exception messages to the client from the request.

--- src/main/java/de/beuth/knabe/spring_ddd_bank/rest/ExceptionAdvice.java
+++ src/main/java/de/beuth/knabe/spring_ddd_bank/rest/ExceptionAdvice.java
@@ -1,10 +1,10 @@
 package de.beuth.knabe.spring_ddd_bank.rest;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.VndErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import multex.Exc;
 import multex.Failure;
 import multex.Msg;
-import multex.Util;
 
 /**
  * Centralized Exception Reporting for all Controller classes.
@@ -25,43 +24,39 @@ import multex.Util;
 @ControllerAdvice
 public class ExceptionAdvice {
 
-	private static final Logger log = LoggerFactory.getLogger(ExceptionAdvice.class);
-	
-	/** Message texts for exceptions. */
-	private final ResourceBundle resourceBundle;
 
-	@Autowired
-	/**
-	 * Constructs the advice with a bundle for localizable message texts for
-	 * exceptions. At the moment the bundle is created as bean
-	 * <code>messageTexts</code> in class
-	 * {@link de.beuth.knabe.spring_ddd_bank.Application}.
-	 */
-	public ExceptionAdvice(final ResourceBundle resourceBundle) {
-		this.resourceBundle = resourceBundle;
-	}
+	/**The baseName for locating the exception message text resource bundle.*/ 
+	public static final String BASE_NAME = "MessageText";
+
+	private static final Logger log = LoggerFactory.getLogger(ExceptionAdvice.class);
 
 	@ResponseBody
 	@ExceptionHandler({Exc.class, Failure.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	VndErrors reportException(final Exception ex) {
-		//prepare messages for client:
-		final StringBuffer out = new StringBuffer();
-		multex.Msg.printMessages(out, ex, resourceBundle);
-		final String messages = out.toString();
-		
-		//prepare log report with messages and stack trace:
-		out.setLength(0);
-		out.append("Processing REST request threw exception:\n");
-		out.append(messages);
-		out.append(Util.lineSeparator);
-		out.append(Msg.stackTraceFollows);
-		out.append(Util.lineSeparator);
-		Msg.printStackTrace(out, ex);
-		
-		//log the report on the server, and respond per messages to the client:
-		log.error(out.toString());
-		return new VndErrors("error", messages);
-	}
+	/**Reports the given Exception with messages localized according to the given Locale of the web request.*/
+	VndErrors reportException(final Exception ex, final Locale requestLocale) {
+		//prepare messages for client with the Locale of the request:
+		/** Message texts for exceptions. */
+		final ResourceBundle requestResourceBundle = ResourceBundle.getBundle(BASE_NAME, requestLocale);
+		final StringBuffer clientMessages = new StringBuffer();
+		multex.Msg.printMessages(clientMessages, ex, requestResourceBundle);
+		final String clientMesagesString = clientMessages.toString();
 
+		//prepare log report with messages and stack trace:
+		final StringBuffer serverMessages = new StringBuffer();
+		serverMessages.append("Processing REST request threw exception:\n");
+		final Locale defaultLocale = Locale.getDefault();
+		final ResourceBundle defaultResourceBundle = ResourceBundle.getBundle(BASE_NAME, defaultLocale);
+		System.out.println("requestResourceBundle: " + requestResourceBundle + ", defaultResourceBundle: " + defaultResourceBundle);
+		if(!defaultResourceBundle.equals(requestResourceBundle)) {
+			serverMessages.append(clientMesagesString);
+			serverMessages.append("\n-----\n");
+		}
+		Msg.printReport(serverMessages, ex, defaultResourceBundle);
+		
+		//log the report on the server:
+		log.error(serverMessages.toString());
+		//respond with localized messages to the client:
+		return new VndErrors("error", clientMesagesString);
+	}
 }
