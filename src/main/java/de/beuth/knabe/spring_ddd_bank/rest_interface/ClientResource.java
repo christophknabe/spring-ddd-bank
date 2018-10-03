7371efed3d117e25bfb81a466848625e4e8317de
Christoph Knabe <knabe@beuth-hochschule.de>
2018-10-03 19:29:20 +0200

    ClientResource annotated for Swagger.

--- src/main/java/de/beuth/knabe/spring_ddd_bank/rest_interface/ClientResource.java
+++ src/main/java/de/beuth/knabe/spring_ddd_bank/rest_interface/ClientResource.java
@@ -1,17 +1,21 @@
 package de.beuth.knabe.spring_ddd_bank.rest_interface;
 
 import de.beuth.knabe.spring_ddd_bank.domain.Client;
+import io.swagger.annotations.ApiModelProperty;
 
 /**Data about a client of a bank. Usable as Data Transfer Object.*/
 public class ClientResource {
 
     /**Unique ID of the client.*/
+	@ApiModelProperty(notes = "The database generated client ID")
 	public Long id;
 
     /**Username of the client. It has to be unique, too.*/
+	@ApiModelProperty(notes = "The username of the client. Must be unique.", required = true)
     public String username;
 
 	/**The birth date of the client in format 31.12.1999.*/
+	@ApiModelProperty(notes = "The birth date of the client in format 31.12.1999", required = true)
     public String birthDate;
 
 
